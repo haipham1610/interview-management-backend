8f76a2bc14ababba5fdb03f546c79fdb613fcb13
Hai Pham <haipdhe172178@fpt.edu.vn>
2024-11-01 11:31:43 +0700

    Initial commit

---
+++ src/test/java/org/backend/interviewmanagement/InterviewManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backend.interviewmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
